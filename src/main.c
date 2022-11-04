3055a9dc1658475fcf08440ba82404805f2ad8a2
Ben Andrews <benafandrews@gmail.com>
2022-11-04 00:33:29 +0000

    Got simple _Generic example working. Need to work out way to do structs

--- src/main.c
+++ src/main.c
@@ -11,18 +11,18 @@
                         printf( "a = %d, b = %c, c = %f\n", x.a, x.b, x.c ) ), \
               GEN_LINE( default, printf( "Unknown type." ) ) )*/
 
-#define PRINT_FUNC( fmt, ... ) printf( fmt, __VA_ARGS__ )
-
 // Formatter incorrectly handles _Generic in current version
 // clang-format off
 
-#define MY_PRINT( x, ... )                                                       \
-    _Generic( ( x ),                                                             \
-            int: PRINT_FUNC( "%d\n", __VA_ARGS__ )                               \
-            char: PRINT_FUNC( "%c\n", __VA_ARGS__ )                              \
-            float: PRINT_FUNC( "%f\n", __VA_ARGS__ )                             \
-            test_struct: PRINT_FUNC( "a = %d, b = %c, c = %f\n", x.a, x.b, x.c ) \
-            default : printf( fmt ) )
+#define FORMAT_SPEC(x) \
+    _Generic( (x), \
+            int: "%d\n", \
+            char: "%c\n", \
+            float: "%f\n" )
+
+#define MY_PRINT( x ) (printf(FORMAT_SPEC(x), x))
+
+// test_struct: PRINT_FUNC( "a = %d, b = %c, c = %f\n", x.a, x.b, x.c ) \
 
 // clang-format on
 
@@ -39,14 +39,13 @@ main() {
     char  b = 'c';
     float c = 3.14;
 
-    test_struct d = {
+    /*test_struct d = {
         .a = 5,
         .b = 'c',
         .c = 3.14,
-    };
+    };*/
 
     MY_PRINT( a );
     MY_PRINT( b );
     MY_PRINT( c );
-    MY_PRINT( d );
 }
